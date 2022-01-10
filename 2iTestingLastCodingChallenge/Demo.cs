36a11f809c5b81cac2d7aa722056b31f49b78468
Depraiser <tosinanddepraiser@gmail.com>
2022-01-10 17:09:38 +0000

    Latest commit

--- 2iTestingLastCodingChallenge/Demo.cs
+++ 2iTestingLastCodingChallenge/Demo.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace _2iTestingLastCodingChallenge
 {
-    internal class Demo
-    {
-    }
+	public class Demo
+	{
+
+		public static void main(String[] args)
+		{
+			
+			int A = 3, X = 9;
+			PrintingMultiples.printMultiples(A, X);
+			PrintingMultiples.printMultiples(A + 1, X + 1);
+			PrintingMultiples.printMultiples(0, 8);
+			PrintingMultiples.printMultiples(-1, -9);
+			PrintingMultiples.printMultiples(2, 18);
+
+		}
+
+	}
 }
--- 2iTestingLastCodingChallenge/PrintingMultiples.cs
+++ 2iTestingLastCodingChallenge/PrintingMultiples.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace _2iTestingLastCodingChallenge
 {
-    internal class PrintingMultiples
-    {
-    }
+  
+
+
+	public class PrintingMultiples
+	{
+
+		public static void printMultiples(int A, int X)
+		{
+			if (A == 0 || A < 0 || X == 0 || X < 0)
+			{
+				Console.Write("Please pass the valid input for A " + A + " and X as " + X);
+			}
+			else
+			{
+				int i = 2;
+				int Number = A;
+				while (Number <= X)
+				{
+					Console.Write(Number + "\t");
+					Number = A * i;
+					i++;
+				}
+				
+			}
+		}
+	}
+
+
+
+
+
+
+
 }
